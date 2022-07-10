940fa27bb017126698f703ecb97d20ff9ed1590f
hwiseong.shin <henry090325@gmail.com>
2022-07-10 17:43:31 +0900

    2869(doing) 1.1.1
    working!

--- 10773.c
+++ 10773.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(void) {
-	
-	
-	
+	int a;
+	int b;
+	int v;
+	int c;
+	int d;
+	scanf("%d %d %d", &a, &b, &v);
+	for(c = 0;d>v;c++){
+		d=d+a-b;
+	}
+	printf("%d", c);
 	return 0;
 }
